396fa1f3ad41ceb3d5bfaafebf2db50742f6310b
sivaroot <sivaroot.sut@gmail.com>
2019-05-21 18:10:11 +0700

    Spring Mono

--- om-backend/src/main/java/com/backend/Om/OmApplication.java
+++ om-backend/src/main/java/com/backend/Om/OmApplication.java
@@ -2,8 +2,6 @@ package com.backend.Om;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class OmApplication /*extends SpringBootServletInitializer*/ {
@@ -11,8 +9,9 @@ public class OmApplication /*extends SpringBootServletInitializer*/ {
 /*	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(OmApplication.class);
-	}
-*/
+	}*/
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(OmApplication.class, args);
 	}
